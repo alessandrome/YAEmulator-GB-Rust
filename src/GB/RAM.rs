c3eb420597405a76bd2d634841f375de81c0ea05
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2024-03-17 17:06:21 +0100

    Added Address locations in RAM/Memory

--- src/GB/RAM.rs
+++ src/GB/RAM.rs
@@ -1,3 +1,13 @@
+pub const RST_INSTRUCIONS: usize = 0x0000;  // Location in RAM for RST instructions (not used on emulation)
+pub const ROM_METADATA: usize = 0x0100;  // Location for ROM metadata (as name) (not used on emulation)
+pub const USER_PROGRAM: usize = 0x0150;  // Location User Program (not used on emulation)
+pub const VRAM_ADDRESS: usize = 0x8000;  // Video RAM
+pub const EXTERNAL_RAM_ADDRESS: usize = 0xA000;  // External Extension RAM
+pub const WRAM_ADDRESS: usize = 0xC000;  // Working RAM
+pub const OAM_RAM_ADDRESS: usize = 0xFE00;  // Up to 40 Display Object Data (512B)
+pub const INTERNAL_RAM_ADDRESS: usize = 0xFF00;  // Instruction Registers & Flags
+pub const HRAM_ADDRESS: usize = 0xFF80;  // High RAM 127B (Memory w/ direct access from CPU)
+
 pub struct RAM {
     memory: [u8; 65536],
 }
