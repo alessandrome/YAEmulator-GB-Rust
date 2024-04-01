28662b7e87c08c5db1369bc755f6a84e622dc39b
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2024-04-01 15:55:40 +0200

    Added subset RLC and RRC instructions

--- src/GB/instructions.rs
+++ src/GB/instructions.rs
@@ -2951,7 +2951,7 @@ const fn create_opcodes() -> [Option<&'static Instruction>; 256] {
         flags: &[],
         execute: |opcode: &Instruction, cpu: &mut CPU| -> u64 {
             // Implement CB subset will be executed on next fetching & execute
-            opcode.cycles as u64
+            cpu.execute_next()
         },
     });
     opcodes[0xCC] = Some(&Instruction {
@@ -3591,31 +3591,97 @@ const fn create_opcodes() -> [Option<&'static Instruction>; 256] {
 }
 
 const fn create_cb_opcodes() -> [Option<&'static Instruction>; 256] {
+    macro_rules! rlc {
+        ($opcode:expr, $name:expr, r8, $set_reg:ident, $get_reg:ident) => {
+            Some(&Instruction {
+                opcode: $opcode,
+                name: $name,
+                cycles: 2,
+                size: 2,
+                flags: &[FlagBits::Z, FlagBits::N, FlagBits::H, FlagBits::C],
+                execute: |opcode: &Instruction, cpu: &mut CPU| -> u64 {
+                    let old_val = cpu.registers.$get_reg();
+                    cpu.registers.set_carry_flag((old_val & 0b1000_0000) != 0);
+                    let new_val = old_val.wrapping_shl(1) | cpu.registers.get_carry_flag() as u8;
+                    cpu.registers.$set_reg(new_val);
+                    cpu.registers.set_zero_flag(new_val == 0);
+                    opcode.cycles as u64
+                }
+            })
+        };
+        ($opcode:expr, $name:expr, ar16, $set_reg:ident, $get_reg:ident) => {
+            Some(&Instruction {
+                opcode: $opcode,
+                name: $name,
+                cycles: 2,
+                size: 2,
+                flags: &[FlagBits::Z, FlagBits::N, FlagBits::H, FlagBits::C],
+                execute: |opcode: &Instruction, cpu: &mut CPU| -> u64 {
+                    let old_val = cpu.ram.read(cpu.registers.$get_reg());
+                    cpu.registers.set_carry_flag((old_val & 0b1000_0000) != 0);
+                    let new_val = old_val.wrapping_shl(1) | cpu.registers.get_carry_flag() as u8;
+                    cpu.ram.write(cpu.registers.$get_reg(), new_val);
+                    cpu.registers.set_zero_flag(new_val == 0);
+                    opcode.cycles as u64
+                }
+            })
+        };
+    }
+    macro_rules! rrc {
+        ($opcode:expr, $name:expr, r8, $set_reg:ident, $get_reg:ident) => {
+            Some(&Instruction {
+                opcode: $opcode,
+                name: $name,
+                cycles: 2,
+                size: 2,
+                flags: &[FlagBits::Z, FlagBits::N, FlagBits::H, FlagBits::C],
+                execute: |opcode: &Instruction, cpu: &mut CPU| -> u64 {
+                    let old_val = cpu.registers.$get_reg();
+                    cpu.registers.set_carry_flag((old_val & 0b0000_0001) != 0);
+                    let new_val = old_val.wrapping_shr(1) | ((cpu.registers.get_carry_flag() as u8) << 7);
+                    cpu.registers.$set_reg(new_val);
+                    cpu.registers.set_zero_flag(new_val == 0);
+                    opcode.cycles as u64
+                }
+            })
+        };
+        ($opcode:expr, $name:expr, ar16, $set_reg:ident, $get_reg:ident) => {
+            Some(&Instruction {
+                opcode: $opcode,
+                name: $name,
+                cycles: 2,
+                size: 2,
+                flags: &[FlagBits::Z, FlagBits::N, FlagBits::H, FlagBits::C],
+                execute: |opcode: &Instruction, cpu: &mut CPU| -> u64 {
+                    let old_val = cpu.ram.read(cpu.registers.$get_reg());
+                    cpu.registers.set_carry_flag((old_val & 0b0000_0001) != 0);
+                    let new_val = old_val.wrapping_shr(1) | ((cpu.registers.get_carry_flag() as u8) << 7);
+                    cpu.ram.write(cpu.registers.$get_reg(), new_val);
+                    cpu.registers.set_zero_flag(new_val == 0);
+                    opcode.cycles as u64
+                }
+            })
+        };
+    }
+
     let mut opcodes = [None; 256];
-    // opcodes[0x00] = Some(&Instruction {
-    //     opcode: 0x00,
-    //     name: "NOP",
-    //     cycles: 1,
-    //     size: 1,
-    //     flags: &[],
-    //     execute: fn a(cpu: &CPU) -> u8,
-    // });
-    // opcodes[0x01] = Some(&Instruction {
-    //     opcode: 0x01,
-    //     name: "LD BC, d16",
-    //     cycles: 3,
-    //     size: 3,
-    //     flags: &[],
-    //     execute: fn a(cpu: &CPU) -> u8,
-    // });
-    // opcodes[0xCB] = Some(&Instruction {
-    //     opcode: 0xCB,
-    //     name: "LD BC, d16",
-    //     cycles: 3,
-    //     size: 3,
-    //     flags: &[],
-    //     execute: fn a(cpu: &CPU) -> u8{},
-    // });
+    opcodes[0x00] = rlc!(0x00, "RLC B", r8, set_b, get_b);
+    opcodes[0x01] = rlc!(0x01, "RLC C", r8, set_c, get_c);
+    opcodes[0x02] = rlc!(0x02, "RLC D", r8, set_d, get_d);
+    opcodes[0x03] = rlc!(0x03, "RLC E", r8, set_e, get_e);
+    opcodes[0x04] = rlc!(0x04, "RLC H", r8, set_h, get_h);
+    opcodes[0x05] = rlc!(0x05, "RLC L", r8, set_l, get_l);
+    opcodes[0x06] = rlc!(0x06, "RLC [HL]", ar16, set_hl, get_hl);
+    opcodes[0x07] = rlc!(0x07, "RLC A", r8, set_a, get_a);
+
+    opcodes[0x08] = rrc!(0x08, "RRC B", r8, set_b, get_b);
+    opcodes[0x09] = rrc!(0x09, "RRC C", r8, set_c, get_c);
+    opcodes[0x0A] = rrc!(0x0a, "RRC D", r8, set_d, get_d);
+    opcodes[0x0B] = rrc!(0x0b, "RRC E", r8, set_e, get_e);
+    opcodes[0x0C] = rrc!(0x0c, "RRC H", r8, set_h, get_h);
+    opcodes[0x0D] = rrc!(0x0d, "RRC L", r8, set_l, get_l);
+    opcodes[0x0E] = rrc!(0x0e, "RRC [HL]", ar16, set_hl, get_hl);
+    opcodes[0x0F] = rrc!(0x0f, "RRC A", r8, set_a, get_a);
     opcodes
 }
 
@@ -9331,4 +9397,122 @@ mod test {
     }
     test_cp_a_imm8!(0xFE, test_0xfe_cp_a_imm8);
     test_rst!(0xFF, test_0xef_rst_38);
+}
+
+#[cfg(test)]
+mod test_cb {
+    use crate::GB::CPU::CPU;
+    use crate::GB::RAM;
+    use crate::GB::RAM::{USER_PROGRAM_ADDRESS, WRAM_ADDRESS};
+
+    macro_rules! test_flags {
+        ($cpu:ident, $zero:expr, $negative:expr, $half:expr, $carry:expr) => {
+            assert_eq!($cpu.registers.get_zero_flag(), $zero);
+            assert_eq!($cpu.registers.get_negative_flag(), $negative);
+            assert_eq!($cpu.registers.get_half_carry_flag(), $half);
+            assert_eq!($cpu.registers.get_carry_flag(), $carry);
+        };
+    }
+
+    macro_rules! test_rlc {
+        ($opcode:expr, $func:ident, $set_reg_src:ident, $get_reg_src:ident) => {
+            #[test]
+            fn $func() {
+                let test_value_1: u8 = 0b1000_1000;
+                let test_addr: u16 = WRAM_ADDRESS as u16 + 0xC6;
+                let mut cpu_1 = CPU::new();
+                let program_1: Vec<u8> = vec![0xCB, $opcode, 0xCB, $opcode];
+                cpu_1.load(&program_1);
+                cpu_1.registers.$set_reg_src(test_value_1);
+                let mut cycles = cpu_1.execute_next();
+                assert_eq!(cycles, 2);
+                assert_eq!(cpu_1.registers.$get_reg_src(), 0b0001_0001);
+                test_flags!(cpu_1, false, false, false, true);
+                cycles = cpu_1.execute_next();
+                assert_eq!(cycles, 2);
+                assert_eq!(cpu_1.registers.$get_reg_src(), 0b0010_0010);
+                test_flags!(cpu_1, false, false, false, false);
+            }
+        };
+        ($opcode:expr, $func:ident, $set_reg_src:ident, $get_reg_src:ident, memory) => {
+            #[test]
+            fn $func() {
+                let test_value_1: u8 = 0b1000_1000;
+                let test_addr: u16 = WRAM_ADDRESS as u16 + 0xC6;
+                let mut cpu_1 = CPU::new();
+                let program_1: Vec<u8> = vec![0xCB, $opcode, 0xCB, $opcode];
+                cpu_1.load(&program_1);
+                cpu_1.ram.write(test_addr, test_value_1);
+                cpu_1.registers.set_hl(test_addr);
+                let mut cycles = cpu_1.execute_next();
+                assert_eq!(cycles, 2);
+                assert_eq!(cpu_1.ram.read(test_addr), 0b0001_0001);
+                test_flags!(cpu_1, false, false, false, true);
+                cycles = cpu_1.execute_next();
+                assert_eq!(cycles, 2);
+                assert_eq!(cpu_1.ram.read(test_addr), 0b0010_0010);
+                test_flags!(cpu_1, false, false, false, false);
+            }
+        };
+    }
+
+    macro_rules! test_rrc {
+        ($opcode:expr, $func:ident, $set_reg_src:ident, $get_reg_src:ident) => {
+            #[test]
+            fn $func() {
+                let test_value_1: u8 = 0b0001_0001;
+                let test_addr: u16 = WRAM_ADDRESS as u16 + 0xC6;
+                let mut cpu_1 = CPU::new();
+                let program_1: Vec<u8> = vec![0xCB, $opcode, 0xCB, $opcode];
+                cpu_1.load(&program_1);
+                cpu_1.registers.$set_reg_src(test_value_1);
+                let mut cycles = cpu_1.execute_next();
+                assert_eq!(cycles, 2);
+                assert_eq!(cpu_1.registers.$get_reg_src(), 0b1000_1000);
+                test_flags!(cpu_1, false, false, false, true);
+                cycles = cpu_1.execute_next();
+                assert_eq!(cycles, 2);
+                assert_eq!(cpu_1.registers.$get_reg_src(), 0b0100_0100);
+                test_flags!(cpu_1, false, false, false, false);
+            }
+        };
+        ($opcode:expr, $func:ident, $set_reg_src:ident, $get_reg_src:ident, memory) => {
+            #[test]
+            fn $func() {
+                let test_value_1: u8 = 0b0001_0001;
+                let test_addr: u16 = WRAM_ADDRESS as u16 + 0xC6;
+                let mut cpu_1 = CPU::new();
+                let program_1: Vec<u8> = vec![0xCB, $opcode, 0xCB, $opcode];
+                cpu_1.load(&program_1);
+                cpu_1.ram.write(test_addr, test_value_1);
+                cpu_1.registers.set_hl(test_addr);
+                let mut cycles = cpu_1.execute_next();
+                assert_eq!(cycles, 2);
+                assert_eq!(cpu_1.ram.read(test_addr), 0b1000_1000);
+                test_flags!(cpu_1, false, false, false, true);
+                cycles = cpu_1.execute_next();
+                assert_eq!(cycles, 2);
+                assert_eq!(cpu_1.ram.read(test_addr), 0b0100_0100);
+                test_flags!(cpu_1, false, false, false, false);
+            }
+        };
+    }
+
+    test_rlc!(0x00, test_0x00_rlc_b, set_b, get_b);
+    test_rlc!(0x01, test_0x01_rlc_c, set_c, get_c);
+    test_rlc!(0x02, test_0x02_rlc_d, set_d, get_d);
+    test_rlc!(0x03, test_0x03_rlc_e, set_e, get_e);
+    test_rlc!(0x04, test_0x04_rlc_h, set_h, get_h);
+    test_rlc!(0x05, test_0x05_rlc_l, set_l, get_l);
+    test_rlc!(0x06, test_0x06_rlc__hl_, set_hl, get_hl, memory);
+    test_rlc!(0x07, test_0x07_rlc_a, set_a, get_a);
+
+    test_rrc!(0x08, test_0x08_rrc_b, set_b, get_b);
+    test_rrc!(0x09, test_0x09_rrc_c, set_c, get_c);
+    test_rrc!(0x0A, test_0x0a_rrc_d, set_d, get_d);
+    test_rrc!(0x0B, test_0x0b_rrc_e, set_e, get_e);
+    test_rrc!(0x0C, test_0x0c_rrc_h, set_h, get_h);
+    test_rrc!(0x0D, test_0x0d_rrc_l, set_l, get_l);
+    test_rrc!(0x0E, test_0x0e_rrc__hl_, set_hl, get_hl, memory);
+    test_rrc!(0x0F, test_0x0f_rrc_a, set_a, get_a);
 }
