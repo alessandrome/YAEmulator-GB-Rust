a821b2f845290827b447827ad9c60cbbb67eeb42
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2024-04-04 16:43:02 +0200

    Added registers memory locations

---
+++ src/GB/memory/registers.rs
@@ -0,0 +1,27 @@
+// Port/Mode Registers
+pub const P1: u16 = 0xFF00;
+pub const SB: u16 = 0xFF01;
+pub const SC: u16 = 0xFF02;
+pub const DIV: u16 = 0xFF03;
+pub const TIMA: u16 = 0xFF04;
+pub const TIMA: u16 = 0xFF05;
+pub const TMA: u16 = 0xFF06;
+pub const TAC: u16 = 0xFF07;
+
+// Interrupt Registers
+pub const IF: u16 = 0xFF0F;
+pub const IE: u16 = 0xFFFF;
+
+// Display Registers
+pub const LCDC: u16 = 0xFF40;
+pub const STAT: u16 = 0xFF41;
+pub const SCY: u16 = 0xFF42;
+pub const SCX: u16 = 0xFF43;
+pub const LY: u16 = 0xFF44;
+pub const LYC: u16 = 0xFF45;
+pub const DNA: u16 = 0xFF46;
+pub const BGP: u16 = 0xFF47;
+pub const OBP0: u16 = 0xFF48;
+pub const OBP1: u16 = 0xFF49;
+pub const WY: u16 = 0xFF4A;
+pub const WX: u16 = 0xFF4B;
