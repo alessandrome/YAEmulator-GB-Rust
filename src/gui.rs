af13f5fde47363e4cae75d843a22d2eee09fc90c
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2024-04-09 20:51:22 +0200

    Added code to show hex commands

--- src/gui.rs
+++ src/gui.rs
@@ -8,6 +8,7 @@ use iced::widget::{Button, Column, Row, Scrollable, Text};
 use iced::{Alignment, Application, Command, Element, Settings};
 use std::fs::File;
 use std::io::Read;
+use crate::GB::CPU::CPU;
 
 // #[derive(Default)]
 // pub enum MainWindow {
@@ -87,9 +88,56 @@ impl Application for MainWindow {
         // container(row).into()
         // let r = row![Text::new("BIOS")];
         // let c = column![r, button("+").on_press(Message::Increment)];
+        let ram = &self.gb.cpu.ram;
+        let ram = &self.gb.rom;
         PaneGrid::new(&self.panes, |pane, state, maximized| {
             let r1 = row![text("BIOS"), text(state.id)];
-            pane_grid::Content::new(r1)
+            let mut c = Column::new().push(r1);
+            for mut i in 0..256 {
+                if state.id == 0 {
+                    let mut opcode = ram.read(i as u16);
+                    let mut hexes = vec![opcode];
+                    let mut hexes_str: String = format!("{:02x}", opcode);
+                    let is_cb = opcode == 0xCB;
+                    if is_cb {
+                        i = i + 1;
+                        opcode = ram.read(i as u16);
+                        hexes.push(opcode);
+                        hexes_str.push_str(&(format!(" {:02x}", opcode)).as_str());
+                    }
+                    let _ins = CPU::decode(&opcode, is_cb);
+                    match (_ins) {
+                        Some(ins) => {
+                            let mut extra: u16 = 0;
+                            if !is_cb {
+                                if ins.size == 2 {
+                                    i += 1;
+                                    extra = ram.read(i as u16) as u16;
+                                    opcode = ram.read(i as u16);
+                                    hexes.push(opcode);
+                                    hexes_str += format!(" {:02x}", opcode).as_str();
+                                } else if ins.size == 3 {
+                                    i += 1;
+                                    extra = ram.read(i as u16) as u16;
+                                    opcode = ram.read(i as u16);
+                                    hexes.push(opcode);
+                                    hexes_str += format!(" {:02x}", opcode).as_str();
+                                    i += 1;
+                                    extra |= (ram.read(i as u16) as u16) << 8;
+                                    opcode = ram.read(i as u16);
+                                    hexes.push(opcode);
+                                    hexes_str += format!(" {:02x}", opcode).as_str();
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                    c = c.push(row![text(hexes_str)]);
+                } else {
+                    c = c.push(row![text("ex")]);
+                }
+            }
+            pane_grid::Content::new(c)
         }).into()
     }
 }
