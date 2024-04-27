fed28143e78ce3a0fdcd46d2324cf762fdb6265d
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2024-04-27 15:18:45 +0200

    Adding attributes and "new" function to OAM

--- src/GB/PPU/oam.rs
+++ src/GB/PPU/oam.rs
@@ -1,3 +1,19 @@
 pub struct OAM {
+    id: Option<usize>, // Optional - Useful to manage as ID of OAM in GB Memory
+    y: u8,
+    x: u8,
+    tile_id: u8,
+    attributes: u8,
+}
 
+impl OAM {
+    pub fn new(y: u8, x: u8, tile_id: u8, attributes: u8, id: Option<usize>) -> Self {
+        Self {
+            id,
+            y,
+            x,
+            tile_id,
+            attributes,
+        }
+    }
 }
