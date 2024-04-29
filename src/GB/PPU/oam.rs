c26d2954f5c692f04353b1f4f07a3b3c503b53b7
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2024-04-29 12:19:47 +0200

    Adding tests for OAM, corrected get_oam_bytes and added original attributes byte passed on new function

--- src/GB/PPU/oam.rs
+++ src/GB/PPU/oam.rs
@@ -11,6 +11,7 @@ pub struct OAM {
     y_flip: bool,
     x_flip: bool,
     palette: bool, // False = OBP0 | True = OBP1
+    original_attributes: u8,
 }
 
 impl OAM {
@@ -28,6 +29,7 @@ impl OAM {
             y_flip,
             x_flip,
             palette,
+            original_attributes: attributes,
         }
     }
 
@@ -35,10 +37,45 @@ impl OAM {
     ///
     /// Order of bytes is (Y, X, Tile ID, Attributes). Structure attributes are converted in its representing byte in memory.
     pub fn get_oam_bytes(&self) -> (u8, u8, u8, u8) {
-        let attributes: u8 = (self.priority as u8) << (AttributesMasks::Priority as u8).leading_zeros()
-            | (self.y_flip as u8) << (AttributesMasks::YFlip as u8).leading_zeros()
-            | (self.x_flip as u8) << (AttributesMasks::XFlip as u8).leading_zeros()
-            | (self.palette as u8) << (AttributesMasks::Palette as u8).leading_zeros();
+        let attributes: u8 = (self.priority as u8) << (AttributesMasks::Priority as u8).trailing_zeros()
+            | (self.y_flip as u8) << (AttributesMasks::YFlip as u8).trailing_zeros()
+            | (self.x_flip as u8) << (AttributesMasks::XFlip as u8).trailing_zeros()
+            | (self.palette as u8) << (AttributesMasks::Palette as u8).trailing_zeros()
+            | (self.original_attributes & 0x0F);
         (self.y, self.x, self.tile_id, attributes)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::GB::PPU::oam::attributes_masks::AttributesMasks;
+    use crate::GB::PPU::oam::OAM;
+
+    #[test]
+    fn new_oam() {
+        let (test_y, test_x, test_tile_id, test_id) = (56u8, 131u8, 33u8, 1usize);
+        let attributes: u8 = 0b1111_0000;
+        let oam = OAM::new(test_y, test_x, test_tile_id, attributes, Option::from(test_id));
+        assert_eq!(oam.y, test_y);
+        assert_eq!(oam.x, test_x);
+        assert_eq!(oam.tile_id, test_tile_id);
+        assert_eq!(oam.id, Some(test_id));
+        assert_eq!(oam.priority, true);
+        assert_eq!(oam.y_flip, true);
+        assert_eq!(oam.x_flip, true);
+        assert_eq!(oam.palette, true);
+    }
+
+    #[test]
+    fn get_oam_bytes() {
+        let (test_y, test_x, test_tile_id, test_id) = (56u8, 131u8, 33u8, None);
+        let attributes: u8 = 0b1001_0110;
+        let oam = OAM::new(test_y, test_x, test_tile_id, attributes, test_id);
+        assert_eq!(oam.id, None);
+        assert_eq!(oam.priority, true);
+        assert_eq!(oam.y_flip, false);
+        assert_eq!(oam.x_flip, false);
+        assert_eq!(oam.palette, true);
+        assert_eq!(oam.get_oam_bytes(), (test_y, test_x, test_tile_id, attributes));
     }
 }
