99fc032d54992a690c86a16e8f37046641e34cc2
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2024-11-04 13:13:03 +0100

    Commented Modes of LCD Register mask enum

--- src/GB/PPU/lcd_stats_masks.rs
+++ src/GB/PPU/lcd_stats_masks.rs
@@ -5,9 +5,9 @@ use crate::{mask_flag_enum_default_impl, default_enum_u8_bit_ops};
 pub enum LCDStatMasks {
     PPUMode = 0b0000_0011,
     LYCeLY = 0b0000_0100,
-    Mode0Interrupt = 0b0000_1000,
-    Mode1Interrupt = 0b0001_0000,
-    Mode2Interrupt = 0b0010_0000,
+    Mode0Interrupt = 0b0000_1000,   // HBlank Mode
+    Mode1Interrupt = 0b0001_0000,   // VBlank Mode
+    Mode2Interrupt = 0b0010_0000,   // OAM Scan Mode
     LYCInterrupt = 0b0100_0000,
 }
 
