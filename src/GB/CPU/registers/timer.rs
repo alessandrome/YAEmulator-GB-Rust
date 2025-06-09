3540cb467c0dabb4fb3e508552ced2f2bb31e24e
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2025-06-09 19:51:40 +0200

    Working on timer registers

--- src/GB/CPU/registers/timer.rs
+++ src/GB/CPU/registers/timer.rs
@@ -1,23 +1,5 @@
 use crate::{define_enum_u8, default_enum_u8_bit_ops, mask_flag_enum_default_impl};
-
-
-define_enum_u8! {
-    pub TACMask {
-        Enabled = 0b0000_0100,
-        TimerClock = 0b0000_0011
-    }
-}
-mask_flag_enum_default_impl!(TACMask);
-
-define_enum_u8! {
-    pub TACClock {
-        M256 =  0b0000_0000,
-        M4 =    0b0000_0001,
-        M16 =   0b0000_0010,
-        M64 =   0b0000_0011
-    }
-}
-mask_flag_enum_default_impl!(TACClock);
+use crate::utils::{falling_edge as utils, falling_edge};
 
 pub const M256_CLOCK_CYCLES: u64 = 256;
 pub const M4_CLOCK_CYCLES: u64 = 4;
@@ -29,9 +11,49 @@ pub const M4_CLOCK_MODE: u8 = 0b01;
 pub const M16_CLOCK_MODE: u8 = 0b10;
 pub const M64_CLOCK_MODE: u8 = 0b11;
 
+define_enum_u8! {
+    pub TACMask {
+        Enabled = 0b0000_0100,
+        TimerClock = 0b0000_0011
+    }
+}
+mask_flag_enum_default_impl!(TACMask);
+
+define_enum_u8! {
+    pub TACClock {
+        M256 =  M256_CLOCK_MODE,
+        M4 =    M4_CLOCK_MODE,
+        M16 =   M16_CLOCK_MODE,
+        M64 =   M64_CLOCK_MODE
+    }
+}
+mask_flag_enum_default_impl!(TACClock);
+
+impl TACClock {
+    pub fn get_timer_bit(&self) -> u8 {
+        match self {
+            Self::M256 => 9,  // 4096 Hz
+            Self::M4 => 3,  // 262144 Hz
+            Self::M16 => 5,  // 65536 Hz
+            Self::M64 => 7,  // 16384 Hz
+            _ => unreachable!(),
+        }
+    }
+    
+    pub fn get_timer_bit_from_u8(bits: u8) -> u8 {
+        match bits {
+            M256_CLOCK_MODE => 9,  // 4096 Hz
+            M4_CLOCK_MODE => 3,  // 262144 Hz
+            M16_CLOCK_MODE => 5,  // 65536 Hz
+            M64_CLOCK_MODE => 7,  // 16384 Hz
+            _ => unreachable!(),
+        }
+    }
+}
+
 pub struct TimerRegisters {
     cycles: u16,
-    div: u8,
+    div_counter: u16, // DIV is composed of 2 8bit-subregister, the lower one is not visible to dev while the upper one is the one commonly named DIV timer
     tima: u8,
     tma: u8,
     tac: u8,
@@ -41,18 +63,41 @@ impl TimerRegisters {
     pub fn new() -> Self {
         Self {
             cycles: 0,
-            div: 0,
+            div_counter: 0,
             tima: 0,
             tma: 0,
             tac: 0,
         }
     }
-    
+
     pub fn tick(&mut self) {
-        let (cycles, overflowed) = self.cycles.overflowing_add(1);
-        self.cycles = cycles;
-        if overflowed {
-            self.tima = self.tma;
+        self.cycles = self.cycles.wrapping_add(1);
+        let old_div_counter = self.div_counter;
+        let old_tma = self.tma;
+        self.div_counter = self.div_counter.wrapping_add(1); // Increment DIV
+        if falling_edge(old_div_counter, self.div_counter, TACClock::get_timer_bit_from_u8(self.tac & TACMask::TimerClock)) {
+            // Change/Increment TMA as needed
+            if (self.tima == 0xFF) {
+                self.tima = self.tma;
+            } else {
+                self.tima += 1;
+            }
         }
+    }
+
+    pub fn div(&self) -> u8 {
+        (self.div_counter >> 8) as u8
+    }
+
+    pub fn reset_div(&mut self) {
+        self.div_counter = 0;
+    }
+
+    pub fn set_tac(&mut self, enabled: bool, clock_mode: TACClock) {
+        self.tac = clock_mode as u8 | (if enabled {0x01} else {0x00} << 2);
+    }
+    
+    pub fn is_tma_active(&self) -> bool {
+        self.tac & TACMask::Enabled != 0
     }
 }
