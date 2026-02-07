df7b4af97cf4c26d141ccb54b173964442f64e91
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2026-02-07 16:15:45 +0100

    Refactor: Adding domain types. Some aliases (as Byte and Word) and some transparent struct (as Address(u16))

---
+++ src/GB/types.rs
@@ -0,0 +1,5 @@
+pub mod address;
+
+pub type AddressRange = u16;
+pub type Byte = u8;
+pub type Word = u16;
---
+++ src/GB/types/address.rs
@@ -0,0 +1,148 @@
+use std::ops::{Add, Sub, BitAnd, BitOr, BitXor, Shl, Shr};
+
+#[repr(transparent)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct Address(pub u16);
+
+impl Address {
+    pub const ZERO: Address = Address(0);
+
+    #[inline]
+    pub fn hi(self) -> u8 {
+        (self.0 >> 8) as u8
+    }
+
+    #[inline]
+    pub fn lo(self) -> u8 {
+        self.0 as u8
+    }
+
+    #[inline]
+    pub fn to_index(self) -> usize {
+        self.0 as usize
+    }
+
+    #[inline]
+    pub fn from_index(index: usize) -> Address {
+        Address(index as u16)
+    }
+}
+
+// ---------------------------------------
+// Operator overloading (as Left operator)
+// ---------------------------------------
+
+impl Add<u16> for Address {
+    type Output = Address;
+    #[inline]
+    fn add(self, rhs: u16) -> Self::Output {
+        Address(self.0.wrapping_add(rhs))
+    }
+}
+
+impl Sub<u16> for Address {
+    type Output = Address;
+    #[inline]
+    fn sub(self, rhs: u16) -> Self::Output {
+        Address(self.0.wrapping_sub(rhs))
+    }
+}
+
+impl BitAnd<u16> for Address {
+    type Output = Address;
+    #[inline]
+    fn bitand(self, rhs: u16) -> Self::Output {
+        Address(self.0 & rhs)
+    }
+}
+
+impl BitOr<u16> for Address {
+    type Output = Address;
+    #[inline]
+    fn bitor(self, rhs: u16) -> Self::Output {
+        Address(self.0 | rhs)
+    }
+}
+
+impl BitXor<u16> for Address {
+    type Output = Address;
+    #[inline]
+    fn bitxor(self, rhs: u16) -> Self::Output {
+        Address(self.0 ^ rhs)
+    }
+}
+
+impl Shl<u32> for Address {
+    type Output = Address;
+    #[inline]
+    fn shl(self, rhs: u32) -> Self::Output {
+        Address(self.0 << rhs)
+    }
+}
+
+impl Shr<u32> for Address {
+    type Output = Address;
+    #[inline]
+    fn shr(self, rhs: u32) -> Self::Output {
+        Address(self.0 >> rhs)
+    }
+}
+
+// ---------------------------------------
+// Operator overloading (as Right operator)
+// ---------------------------------------
+impl Add<Address> for u16 {
+    type Output = Address;
+    #[inline]
+    fn add(self, rhs: Address) -> Self::Output {
+        Address(self.wrapping_add(rhs.0))
+    }
+}
+
+impl Sub<Address> for u16 {
+    type Output = Address;
+    #[inline]
+    fn sub(self, rhs: Address) -> Self::Output {
+        Address(self.wrapping_sub(rhs.0))
+    }
+}
+
+impl BitAnd<Address> for u16 {
+    type Output = Address;
+    #[inline]
+    fn bitand(self, rhs: Address) -> Self::Output {
+        Address(self & rhs.0)
+    }
+}
+
+impl BitOr<Address> for u16 {
+    type Output = Address;
+    #[inline]
+    fn bitor(self, rhs: Address) -> Self::Output {
+        Address(self | rhs.0)
+    }
+}
+
+impl BitXor<Address> for u16 {
+    type Output = Address;
+    #[inline]
+    fn bitxor(self, rhs: Address) -> Self::Output {
+        Address(self ^ rhs.0)
+    }
+}
+
+impl Shl<Address> for u16 {
+    type Output = Address;
+    #[inline]
+    fn shl(self, rhs: Address) -> Self::Output {
+        Address(self << rhs.0)
+    }
+}
+
+impl Shr<Address> for u16 {
+    type Output = Address;
+    #[inline]
+    fn shr(self, rhs: Address) -> Self::Output {
+        Address(self >> rhs.0)
+    }
+}
