aceb35ec134c56e815f46127db66318f4f029585
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2026-02-16 11:09:35 +0100

    Refactor - Register macro with inline directive

--- src/GB/CPU/registers/macro_registers.rs
+++ src/GB/CPU/registers/macro_registers.rs
@@ -1,9 +1,11 @@
 macro_rules! get_set {
     ($reg:ident, $get_name:ident, $set_name:ident, $size:ty) => {
+        #[inline]
         pub fn $get_name(&self) -> $size {
             self.$reg
         }
 
+        #[inline]
         pub fn $set_name(&mut self, val: $size) {
             self.$reg = val;
         }
@@ -13,10 +15,12 @@ macro_rules! get_set {
 #[macro_export]
 macro_rules! get_set_dual {
     ($reg1:ident, $reg2:ident, $get_name:ident, $set_name:ident) => {
+        #[inline]
         pub fn $get_name(&self) -> u16 {
             (self.$reg1 as u16) << 8 | self.$reg2 as u16
         }
 
+        #[inline]
         pub fn $set_name(&mut self, val: u16) {
             self.$reg1 = (val >> 8) as u8;
             self.$reg2 = val as u8;
@@ -27,6 +31,7 @@ macro_rules! get_set_dual {
 #[macro_export]
 macro_rules! get_set_flag {
     ($get_name:ident, $set_name:ident, $flag:ident) => {
+        #[inline]
         pub fn $get_name(&self) -> bool {
             (FlagBits::$flag & self.f) != 0
         }
