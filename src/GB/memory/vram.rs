914f21c5feca7ceeccc2a79bb426e3cd0945faa1
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2026-02-28 17:10:13 +0100

    PPU - VRAM implemented tile_id which return a tile map byte containing a tile id

--- src/GB/memory/vram.rs
+++ src/GB/memory/vram.rs
@@ -1,5 +1,5 @@
 use crate::GB::bus::BusDevice;
-use crate::GB::ppu::tile::{Tile, TileDataArea};
+use crate::GB::ppu::tile::{Tile, TileDataArea, TileMapArea};
 use super::{Length, Memory};
 use crate::GB::types::address::{Address, AddressRangeInclusive};
 use crate::GB::types::Byte;
@@ -21,11 +21,14 @@ impl VRAM {
     pub const VRAM_TILE_BLOCK_2_START: Address = Address(0x9000);
     pub const VRAM_TILE_BLOCK_2_END: Address = Address(0x97FF);
     pub const VRAM_TILE_BLOCK_2_RANGE: AddressRangeInclusive = Self::VRAM_TILE_BLOCK_2_START..=Self::VRAM_TILE_BLOCK_2_END;
-    pub const VRAM_TILE_MAP_START: Address = Address(0x9800);
-    pub const VRAM_TILE_MAP_END: Address = Address(0x9FFF);
-    pub const VRAM_TILE_MAP_RANGE: AddressRangeInclusive = Self::VRAM_TILE_MAP_START..=Self::VRAM_TILE_MAP_END;
+    pub const VRAM_TILE_MAP_0_START: Address = Address(0x9800);
+    pub const VRAM_TILE_MAP_0_END: Address = Address(0x9BFF);
+    pub const VRAM_TILE_MAP_0_RANGE: AddressRangeInclusive = Self::VRAM_TILE_MAP_0_START..=Self::VRAM_TILE_MAP_0_END;
+    pub const VRAM_TILE_MAP_1_START: Address = Address(0x9C00);
+    pub const VRAM_TILE_MAP_1_END: Address = Address(0x9FFF);
+    pub const VRAM_TILE_MAP_1_RANGE: AddressRangeInclusive = Self::VRAM_TILE_MAP_1_START..=Self::VRAM_TILE_MAP_1_END;
     pub const VRAM_START_ADDRESS: Address = Self::VRAM_TILE_BLOCK_0_START; // Video memory
-    pub const VRAM_END_ADDRESS: Address = Self::VRAM_TILE_MAP_END; // Video memory
+    pub const VRAM_END_ADDRESS: Address = Self::VRAM_TILE_MAP_1_END; // Video memory
     pub const VRAM_ADDRESS_RANGE: AddressRangeInclusive = Self::VRAM_START_ADDRESS..=Self::VRAM_END_ADDRESS; // Video memory
 
     pub fn new() -> Self {
@@ -45,13 +48,29 @@ impl VRAM {
             tile_address_summer = id as u16;
             base_tile_idx = Self::VRAM_TILE_BLOCK_0_START.as_index() - Self::VRAM_START_ADDRESS.as_index();
         } else {
-            tile_address_summer = (id as u16 + 128) % 256;
+            tile_address_summer = (id as u16 + 128) & 0xFF; // -- AND 0xFF is like do % 256 but faster
             base_tile_idx = Self::VRAM_TILE_BLOCK_1_START.as_index() - Self::VRAM_START_ADDRESS.as_index();
         }
 
         let memory_idx = base_tile_idx + tile_address_summer as usize * Tile::TILE_SIZE as usize;
         let slice = &self.memory[memory_idx..memory_idx + Tile::TILE_SIZE as usize];
         Tile::from_bytes(<&[Byte; 16]>::try_from(slice).expect("Expecting 16 bytes"))
+    }
+
+    #[inline]
+    /// Retrieve ID of a tile from the specified Tile Map Are given the ID position in it.
+    /// The Id position value is ANDed (&) with 0x3FF value to ensure value between 0-1023
+    pub fn tile_id(&self, id: u16, map_area: TileMapArea) -> Byte {
+        match map_area {
+            TileMapArea::MapBlock0 => {
+                let base_index = Self::VRAM_TILE_BLOCK_0_START.as_usize() - Self::VRAM_START_ADDRESS.as_usize();
+                self.memory[base_index + (id & 0x3FF) as usize]
+            }
+            TileMapArea::MapBlock1 => {
+                let base_index = Self::VRAM_TILE_BLOCK_1_START.as_usize() - Self::VRAM_START_ADDRESS.as_usize();
+                self.memory[base_index + (id & 0x3FF) as usize]
+            }
+        }
     }
 }
 
