34ad6e9f40a6b628d3edb36f74e4276e9c08e5b6
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2026-03-03 18:06:51 +0100

    PPU - Starting on Drawing Mode PPU now count pixel to discard with the first BG Pixel FIFO

--- src/GB/ppu.rs
+++ src/GB/ppu.rs
@@ -37,23 +37,6 @@ pub mod palette;
 pub mod pixel_fetcher;
 pub mod tile_line;
 
-macro_rules! ppu_get_set_flag_bit {
-    ($get_func: ident, $set_func: ident, $register_ident: ident, $mask_ident: expr) => {
-        pub fn $get_func(&self) -> bool {
-            (self.read_memory($register_ident as u16) & $mask_ident) != 0
-        }
-        pub fn $set_func(&mut self, flag: bool) {
-            let flag_byte = self.read_memory($register_ident as u16);
-            let base_mask = !$mask_ident as u8;
-            let bit_num = base_mask.trailing_ones();
-            self.write_memory(
-                $register_ident as u16,
-                flag_byte & base_mask | ((flag as u8) << bit_num),
-            );
-        }
-    };
-}
-
 const SCREEN_LINES: u16 = 144;
 const SCREEN_COLUMNS: u16 = 160;
 const OAM_BUFFER: u8 = 10;
@@ -63,6 +46,7 @@ pub struct PPU {
     pixel_fetcher: PixelFetcher,
     oam_loading: Vec<Byte>,
     oam_scans: u8,
+    discarding_pixels: u8,
     dot: u16,
     dots_penalties: u8,
     dots_penalties_counter: u8,
@@ -86,153 +70,12 @@ impl PPU {
             pixel_fetcher: PixelFetcher::new(),
             oam_loading: Vec::with_capacity(OAM::OAM_BYTES as usize),
             oam_scans: 0,
+            discarding_pixels: 0,
             dot: 0,
             dots_penalties: 0,
             dots_penalties_counter: 0,
             screen_dot: 0,
             switch_mode: false,
-        }
-    }
-
-    /// Execute a cycle of PPU. Each cycle is the equivalent of 1 Dot.
-    ///
-    /// Drawing penalties are emulated doing nothing during them. Theme are then added to HBlank mode to reduce its available dots.
-    pub fn _tick(&mut self) {
-        const SCAN_OAM_DOTS_END: usize = constants::SCAN_OAM_DOTS - 1;
-        const DRAW_DOTS_END: usize = constants::DRAW_LINE_MAX_DOTS - 1 + constants::SCAN_OAM_DOTS;
-        const HBLANK_DOTS_START: usize = DRAW_DOTS_END + 1;
-        const HBLANK_DOTS_END: usize = HBLANK_DOTS_START + constants::HBLANK_MIN_DOTS - 1;
-
-        // Get line, check if we are counting penalties, increment line DOT (and line if needed)
-        let mut line = self.get_line() as usize;
-
-        // Execute
-        if line > constants::SCREEN_HEIGHT - 1 {
-            if line == constants::SCREEN_HEIGHT && self.line_dots == 0 {
-                self.set_mode(PpuMode::VBlank);
-                let mut old_if = self.memory.borrow().read(memory::registers::IF) | InterruptFlagsMask::VBlank;
-                // Check if VBlank Interrupt mode is enabled on STAT register
-                let stat_reg = self.memory.borrow().read(memory::registers::STAT);
-                if (stat_reg & LCDStatMasks::Mode1Interrupt) != 0 {
-                    old_if = old_if | InterruptFlagsMask::LCD;
-                }
-                self.memory.borrow_mut().write(memory::registers::IF, old_if);
-            }
-        } else {
-            let scx = self.read_memory(addresses::SCX_ADDRESS as u16) as usize;
-            let scy = self.read_memory(addresses::SCY_ADDRESS as u16) as usize;
-            if self.line_dots == SCAN_OAM_DOTS {
-                // Just entered in draw mode
-                self.dots_penalties += scx % 8;
-            }
-
-            match self.line_dots {
-                // Read OAM data to retrieve line sprites
-                0..=SCAN_OAM_DOTS_END => {
-                    // You can scan a maximum of 40 OAMs and a maximum of 10 OAMs per line
-                    if self.line_dots < constants::OAM_NUMBERS && self.line_oam.len() < constants::MAX_SPRITE_PER_LINE {
-                        let line_isize = line as isize;
-                        let tile_mod = self.get_tile_mode();
-                        let oam = self.get_oam(self.line_dots);
-                        let oam_y_screen = oam.get_y_screen();
-                        let tile_height = TILE_HEIGHT * (tile_mod as usize + 1); // If dual tile sprite is enabled sprite has doubled the height
-                        if oam_y_screen <= line_isize && (oam_y_screen + tile_height as isize) > line_isize {
-                            self.line_oam.push(oam);
-                        }
-                    }
-                }
-                // Update pixels of the frame
-                constants::SCAN_OAM_DOTS..=DRAW_DOTS_END => {
-                    if self.screen_dot < SCREEN_WIDTH {
-                        let screen_pixel_index = self.screen_dot + line * SCREEN_WIDTH;
-                        let is_bg_enabled = self.is_bg_win_enabled();
-                        let is_sprite_enabled = self.is_obj_enabled();
-                        let mut pixel_set = false;
-                        if is_sprite_enabled && self.line_oam_number < self.line_oam.len() {
-                            let oam = &self.line_oam[self.line_oam_number];
-                            let obj_dot = self.screen_dot as isize - oam.get_x_screen();
-                            if obj_dot >= 0 && obj_dot < TILE_WIDTH as isize {
-                                let obj_line = line as isize - oam.get_y_screen();
-                                if obj_line >= 0 && obj_line < TILE_HEIGHT as isize {
-                                    let tile = self.get_tile(oam.get_tile_id(), false);
-                                    let tile_pixel_index = obj_dot + obj_line * TILE_WIDTH as isize;
-                                    let pixel = tile.tile_map()[tile_pixel_index as usize].clone();
-                                    if pixel != GbPaletteId::Id0 {
-                                        self.frame[screen_pixel_index] = pixel;
-                                        pixel_set = true;
-                                    }
-                                }
-                            }
-                        } else if is_bg_enabled && !pixel_set {
-                            let tile = self.get_tile(
-                                self.get_bg_chr(self.get_bg_chr_id(self.screen_dot as u8, line as u8)),
-                                true);
-                            let x_tile = self.get_bg_x() as usize % TILE_WIDTH;
-                            let y_tile = self.get_bg_y() as usize % TILE_HEIGHT;
-                            self.frame[screen_pixel_index] = tile.tile_map()[x_tile + y_tile * TILE_WIDTH].clone();
-                        } else {
-                            self.frame[screen_pixel_index] = GbPaletteId::Id0;
-                        }
-                        self.screen_dot += 1;
-                    }
-                }
-                // During HBlank PPU is doing nothing
-                _ => {
-
-                }
-            }
-        }
-
-        // Update
-        if self.dots_penalties_counter > 0 {
-            self.dots_penalties_counter -= 1;
-        } else {
-            self.line_dots = (self.line_dots + 1) % constants::LINE_DOTS;
-            match self.line_dots {
-                0 => {
-                    self.set_mode(PpuMode::OAMScan);
-                    // Check if HBlank Interrupt mode is enabled on STAT register
-                    let stat_reg = self.memory.borrow().read(memory::registers::STAT);
-                    if (stat_reg & LCDStatMasks::Mode2Interrupt) != 0 {
-                        let old_if = self.memory.borrow().read(memory::registers::IF);
-                        self.memory.borrow_mut().write(memory::registers::IF, old_if | InterruptFlagsMask::LCD);
-                    }
-                    line += 1;
-                    line %= constants::FRAME_LINES;
-                    self.write_memory(addresses::LY_ADDRESS as u16, line as u8);
-                }
-                constants::SCAN_OAM_DOTS => {
-                    self.set_mode(PpuMode::Drawing);
-                    self.line_oam.sort();
-                }
-                HBLANK_DOTS_START => {
-                    self.line_dots += self.dots_penalties;
-                    self.set_mode(PpuMode::HBlank);
-                    // Check if HBlank Interrupt mode is enabled on STAT register
-                    let stat_reg = self.memory.borrow().read(memory::registers::STAT);
-                    if (stat_reg & LCDStatMasks::Mode0Interrupt) != 0 {
-                        let old_if = self.memory.borrow().read(memory::registers::IF);
-                        self.memory.borrow_mut().write(memory::registers::IF, old_if | InterruptFlagsMask::LCD);
-                    }
-                    self.dots_penalties = 0;
-                    self.line_oam.clear();
-                    self.line_oam_number = 0;
-                    self.screen_dot = 0;
-                }
-                _ => {}
-            }
-        }
-
-        // Update STAT register LY == LYC bit and
-        let stat_reg = self.memory.borrow().read(memory::registers::STAT);
-        if line as u8 == self.get_line_compare() {
-            self.memory.borrow_mut().write(memory::registers::STAT, stat_reg | LCDStatMasks::LYCeLY);
-            if (stat_reg & LCDStatMasks::LYCInterrupt) != 0 {
-                let old_if = self.memory.borrow().read(memory::registers::IF);
-                self.memory.borrow_mut().write(memory::registers::IF, old_if | InterruptFlagsMask::LCD);
-            }
-        } else {
-            self.memory.borrow_mut().write(memory::registers::STAT, stat_reg & !LCDStatMasks::LYCeLY);
         }
     }
 }
@@ -258,7 +101,9 @@ impl Tick for PPU {
                     self.dots_penalties_counter = 0;
                     self.screen_dot = 0;
                 }
-                PpuMode::Drawing => {}
+                PpuMode::Drawing => {
+                    self.discarding_pixels = ctx.ppu_mmio.scx() & 7;
+                }
                 PpuMode::HBlank => {}
                 PpuMode::VBlank => {}
             }
@@ -298,14 +143,18 @@ impl Tick for PPU {
             }
             PpuMode::Drawing => {
                 // Mode 3 - Drawing Pixels
+                let discard_pixels = self.discarding_pixels > 0;
+                if discard_pixels {
+                    if ctx.ppu_mmio.pop_bg_pixel().is_some() {
+                        self.discarding_pixels -= 1;
+                    }
+                } else {
+                    todo!("LCD Tick - Mix & Push pixel to screen (if BG ready!)");
+                }
+
                 self.pixel_fetcher.tick(bus, ctx);
 
-                // Penalities
-                if self.dots_penalties_counter < self.dots_penalties {
-                    self.dots_penalties_counter += 1;
-                } else {
-                    self.screen_dot += 1;
-                }
+                // Penalities - Now are already included in Pixel FIFO Behavior
             }
             PpuMode::HBlank => {
                 // Mode 0 - HBlank
