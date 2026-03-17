5b407d484509853b377aeb10ee42673591eb961d
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2026-03-17 09:08:22 +0100

    ROM - Header: added Rom and Ram size addresses

--- src/GB/cartridge/header.rs
+++ src/GB/cartridge/header.rs
@@ -17,14 +17,16 @@ impl RomHeader {
     pub const HEADER_SIZE: usize = Self::HEADER_END_ADDRESS.as_usize() - HEADER_START_ADDRESS.as_usize();
     pub const HEADER_TITLE_START_ADDRESS: Address = Address(0x0134);
     pub const HEADER_TITLE_END_ADDRESS: Address = Address(0x0143);
+    pub const HEADER_ROM_SIZE_ADDRESS: Address = Address(0x0148);
+    pub const HEADER_RAM_SIZE_ADDRESS: Address = Address(0x0149);
 }
 
 impl RomHeader {
     pub fn new(header_slice: &[Byte; Self::HEADER_SIZE]) -> Self {
         let title_result = String::from_utf8(
-            &header_slice[
+            Vec::from(&header_slice[
                 (Self::HEADER_TITLE_START_ADDRESS.as_usize() - Self::HEADER_START_ADDRESS.as_usize())..=(Self::HEADER_TITLE_START_ADDRESS.as_usize() - Self::HEADER_END_ADDRESS.as_usize())
-                ]
+                ])
         ).unwrap();
         Self {
             raw_header: header_slice.clone(),
