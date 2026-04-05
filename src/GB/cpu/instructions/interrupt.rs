539a82132abf51865a94ecb2c2cd7ffa10bce854
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2026-04-05 23:43:48 +0200

    Interrupt - Added method and associated functions to InterruptType (in interrupt.rs) to get reference to that interrupt Instruction item

--- src/GB/cpu/instructions/interrupt.rs
+++ src/GB/cpu/instructions/interrupt.rs
@@ -1,3 +1,5 @@
+use crate::GB::cpu::instructions::{self, Instruction};
+
 #[derive(Copy, Clone, Debug)]
 pub enum InterruptType {
     VBlank,
@@ -5,4 +7,20 @@ pub enum InterruptType {
     Timer,
     Serial,
     Joypad,
+}
+
+impl InterruptType {
+    pub fn interrupt_instruction(interrupt_type: InterruptType) -> &'static Instruction {
+        match interrupt_type {
+            InterruptType::VBlank => &instructions::INTERRUPT_VBLANK,
+            InterruptType::LCD => &instructions::INTERRUPT_LCD,
+            InterruptType::Timer => &instructions::INTERRUPT_TIMER,
+            InterruptType::Serial => &instructions::INTERRUPT_SERIAL,
+            InterruptType::Joypad => &instructions::INTERRUPT_JOYPAD,
+        }
+    }
+
+    pub fn instruction(&self) -> &'static Instruction {
+        Self::interrupt_instruction(*self)
+    }
 }
